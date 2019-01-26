22d2cae1cb69eb1e2f74f175fcd8ac15dd807639
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-01-26 07:07:40 -0300

    feat:add a new screen with instructions about game

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Diogenes Dauster on 1/26/19.
+//  Copyright © 2019 Diogenes Dauster. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
